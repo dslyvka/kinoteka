ad7418c2aad72fc9fa594fd2c4ff555921e4b061
Viacheslav Kovalov <viacheslav.kovalov1@gmail.com>
2021-11-20 15:58:27 +0200

    fix updatePage

--- src/js/onHomeRendering.js
+++ src/js/onHomeRendering.js
@@ -1,9 +1,11 @@
-import renderPopularMovies from "./renderPopularMovies.js";
-
 const refs = {
     logoBtn: document.querySelector('.js-button__logo'),
     homeBtn: document.querySelector('.js-button__home'),
 };
 
-refs.logoBtn.addEventListener('click', renderPopularMovies);
-refs.homeBtn.addEventListener('click', renderPopularMovies);
+refs.logoBtn.addEventListener('click', toReloadPage);
+refs.homeBtn.addEventListener('click', toReloadPage);
+
+function toReloadPage() {
+    window.location.reload();
+}
