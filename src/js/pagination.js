2a19d52c3c2bc5be5a1da8f9dc55b5dcfdc47b1b
Sup-Maryna <1979@ukr.net>
2021-11-20 13:57:33 +0200

    add pagination (html, js, scss)

---
+++ src/js/pagination.js
@@ -0,0 +1,18 @@
+import Pagination from 'tui-pagination';
+
+const ITEMS_PER_PAGE_HOME = 20;
+
+export function createPagination() {
+  const options = {
+    itemsPerPage: ITEMS_PER_PAGE_HOME,
+    visiblePages: 5,
+    page: 1,
+    centerAlign: true,
+    firstItemClassName: 'tui-first-child',
+    lastItemClassName: 'tui-last-child',
+    usageStatistics: false,
+  };
+
+  const container = document.getElementById('tui-pagination-container');
+  window.pagination = new Pagination(container, options);
+}
