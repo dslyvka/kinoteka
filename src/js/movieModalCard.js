5b70a09797582e6a8fcc9cc5b4536a6b84c2f704
yuriykolisnyk <ykolisnyk@gmail.com>
2021-11-22 13:54:40 +0200

    Add modalCard

--- src/js/movieModalCard.js
+++ src/js/movieModalCard.js
@@ -1,6 +1,55 @@
 // JS модального окна с полной информацией о кинофильме
 
-// В процессе
-
-// пока ссылка на импорт шаблона карточки
 import movieModalCard from '../templates/movieModalCardTemplate.hbs';
+import * as basicLightbox from 'basiclightbox';
+import 'basiclightbox/dist/basicLightbox.min.css';
+
+const apiKey = '6a2ef13a57616b6abb93fc4394172b01';
+
+const cardMovie = document.querySelector('.collection');
+
+cardMovie.addEventListener('click', openModal);
+
+function fetchOneMovieInfo(movie_id) {
+  const url = `https://api.themoviedb.org/3/movie/${movie_id}?api_key=${apiKey}`;
+  return fetch(url)
+    .then(response => response.json())
+    .then(data => ({
+      ...data,
+      popularity: data.popularity.toFixed(1),
+    }));
+}
+
+function openModal(e) {
+  e.preventDefault();
+
+  fetchOneMovieInfo(e.target.dataset.id)
+    .then(data => {
+      if (e.target.nodeName !== 'IMG') return;
+
+      const markup = movieModalCard(data);
+      const modal = basicLightbox.create(markup);
+
+      modal.show();
+
+      const closeButton = document.querySelector('.modal__button-close');
+      closeButton.addEventListener('click', closeModal);
+      window.addEventListener('keydown', closeModalHandler);
+
+      function closeModalHandler(e) {
+        if (e.code === 'Escape') {
+          modal.close(e);
+          window.removeEventListener('keydown', closeModalHandler);
+        }
+      }
+
+      function closeModal(e) {
+        modal.close();
+        window.removeEventListener('keydown', closeModalHandler);
+      }
+    })
+    .then(data => {})
+    .catch(error => {
+      console.log();
+    });
+}
