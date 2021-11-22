4fdc9e67b57eb4ec2ea5aaa2bd6683f081c0b8b7
IhorShvydkyi <igorshvydky35@gmail.com>
2021-11-22 21:47:45 +0200

    currentBtn

--- src/js/isHiddenForHeaderBtns.js
+++ src/js/isHiddenForHeaderBtns.js
@@ -5,6 +5,7 @@ const refs = {
   logoBtn: document.querySelector('.js-button__logo'),
   homeBtn: document.querySelector('.js-button__home'),
   myLibraryBtn: document.querySelector('.js-button__myLibrary'),
+  watchedBtn: document.querySelector('.js-button-watched'),
 };
 
 function toggleClass(refsRemove, refsAdd, classHiden) {
@@ -17,7 +18,7 @@ refs.myLibraryBtn.addEventListener('click', onMyLibraryBtnClick);
 function onHomeBtnClick() {
   toggleClass(refs.formSearch, refs.navLibrary, 'visually-hidden');
   toggleClass(refs.myLibraryBtn, refs.homeBtn, 'current-page');
-  refs.header.classList.remove('header_myLibrary');
+  refs.header.classList.remove('header__myLibrary');
   refs.header.classList.add('header__bg-home')
 
 }
@@ -26,5 +27,6 @@ function onMyLibraryBtnClick() {
   toggleClass(refs.navLibrary, refs.formSearch, 'visually-hidden');
   toggleClass(refs.homeBtn, refs.myLibraryBtn, 'current-page');
   refs.header.classList.add('header__myLibrary');
-  refs.header.classList.remove('header__bg-home')
+  refs.header.classList.remove('header__bg-home');
+  refs.watchedBtn.classList.add('button__current');
 }
