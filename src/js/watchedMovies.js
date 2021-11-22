705b3abdab00fbfe1ac5924890e1ae6cdcc69a5f
dslyvka <daniilslyvka@gmail.com>
2021-11-22 20:09:35 +0300

    watched local

--- src/js/watchedMovies.js
+++ src/js/watchedMovies.js
@@ -1,21 +1,32 @@
-// const watched = [];
-// let myLib = document.querySelector('.collection');
-// let markup = '';
+const key = '6a2ef13a57616b6abb93fc4394172b01';
+const watched = [];
+let myLib = document.querySelector('.collection');
+let markup = '';
+import cardTemplateWatched from '../templates/cardsTemplateWatched.hbs';
 
-// // Слушатель по клику на кнопку добавляет фильм в массив объектов
+// https://api.themoviedb.org/3/movie/{movie_id}?api_key=<<api_key>>&language=en-US
 
-// setTimeout(() => {
-//   let movieMarkup = document.querySelector('.collection__item').outerHTML;
-//   let movieMarkup2 = document.querySelector('.collection__item').outerHTML;
+// Слушатель по клику на кнопку добавляет фильм в массив объектов
 
-//   watched.unshift(movieMarkup);
-//   watched.unshift(movieMarkup2);
+export default async function addToWatched() {
+  const movie = document.querySelector('.modal-card__img');
+  const id = movie.dataset.id;
+    const query = `https://api.themoviedb.org/3/movie/${id}?api_key=${key}&language=en-US`;
+    let res;
+    await fetch(query).then(data => data.json()).then(data => res = cardTemplateWatched(data));
+    if (!watched.includes(res)) watched.unshift(res);
+    console.log(res);
+    console.log(watched);
+}
 
-//   localStorage.setItem('watched', JSON.stringify(watched));
-//   console.log(watched);
-//   const watchedLocal = JSON.parse(localStorage.getItem('watched'));
-//   watchedLocal.forEach(element => {
-//     markup += element;
-//   });
-//   myLib.insertAdjacentHTML('beforeend', markup);
-// }, 250);
+
+// fetch(
+//   'https://api.themoviedb.org/3/movie/?api_key=6a2ef13a57616b6abb93fc4394172b01&language=en-US',
+// )
+//   .then(data => data.json())
+//   .then(console.log);
+
+// import addToWatched from './watchedMovies';
+
+    //   const a = document.querySelector('.js-addWatched');
+    //   a.addEventListener('click', addToWatched);
